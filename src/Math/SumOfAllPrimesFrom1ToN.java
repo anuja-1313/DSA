16a9df6e7fabfb2737df81169e76d8c891ed166e
Anuja <anujajadhav0704@gmail.com>
2025-04-19 18:10:35 +0530

    Math (Sieve of Eratosthenes) - Sum of all prime numbers between 1 and n - Easy

--- src/Math/SumOfAllPrimesFrom1ToN.java
+++ src/Math/SumOfAllPrimesFrom1ToN.java
@@ -1,11 +1,44 @@
 package Math;
+import java.util.*;
 
 public class SumOfAllPrimesFrom1ToN {
-    /*
+
+//Sieve of Eratosthenes
+//TC: O(N log log N)
+//SC: O(N) , arrPrime array
+    class Solution {
+        public int prime_Sum(int n) {
+            // code here
+            boolean[] arrPrime = new boolean[n+1];
+            Arrays.fill(arrPrime, true);
+            arrPrime[0] = arrPrime[1] = false;
+
+            for(int i=2; i*i<=n; i++){
+                if(arrPrime[i]){
+                    for(int j = i*i; j<=n; j = j + i){
+                        arrPrime[j] = false;
+                    }
+                }
+            }
+
+            int sum = 0;
+            for(int i=2; i<=n; i++){
+                if(arrPrime[i]){
+                    sum = sum + i;
+                }
+            }
+
+            return sum;
+        }
+    }
+
+/*
 Sum of all prime numbers between 1 and n - Easy
 TC: O(n√n)
 SC: O(1)
 */
+
+    /*
     class Solution {
         public int prime_Sum(int n) {
             // code here
@@ -26,4 +59,5 @@ SC: O(1)
             return true;
         }
     }
+    */
 }
