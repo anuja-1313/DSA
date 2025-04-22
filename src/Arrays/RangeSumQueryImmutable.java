f507fedf65ddb8f0ecc147b5a3eee274649b2e6a
Anuja <anujajadhav0704@gmail.com>
2025-04-22 12:51:49 +0530

    Arrays, Prefix Sum - 303. Range Sum Query - Immutable  (Easy)

---
+++ src/Arrays/RangeSumQueryImmutable.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class RangeSumQueryImmutable {
+
+/*
+303. Range Sum Query - Immutable  (Easy)
+TC: O(N)
+SC: O(N)
+*/
+    class NumArray {
+        private int[] prefix;
+
+        public NumArray(int[] nums) {
+            prefix = new int[nums.length];
+            prefix[0] = nums[0];
+            for(int i=1; i<nums.length; i++){
+                prefix[i] = prefix[i-1] + nums[i];
+            }
+        }
+
+        public int sumRange(int left, int right) {
+            if(left > 0){
+                return prefix[right] - prefix[left - 1];
+            }
+            return prefix[right];
+        }
+    }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+}
